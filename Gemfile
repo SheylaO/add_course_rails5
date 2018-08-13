0a2abcec68071232940776bb72745358620b0d6b
SheylaO <sheyla.m.ortiz@gmail.com>
2018-08-12 21:14:26 -0500

    vImplementing delete article

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'jquery-rails'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
+#gem adding font awesome 
+gem "font-awesome-rails"
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -18,16 +21,13 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
-    @article = Article.find(params[:id])
+  def show 
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:success] = "Article has been updated"
       redirect_to article_path(@article)
@@ -36,6 +36,14 @@ class ArticlesController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    if @article.destroy
+        flash[:success] = "Article has been deleted"
+        redirect_to articles_path
+    end
+  end
+
 
   protected
 
@@ -48,7 +56,12 @@ class ArticlesController < ApplicationController
 
 
   private
-   def article_params
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
      params.require(:article).permit(:title, :body)
-   end
+  end
 end
