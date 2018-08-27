34bc4f4a0dac5cd53d0efed6a209c00a31c3b041
SheylaO <sheyla.m.ortiz@gmail.com>
2018-08-27 18:38:27 -0500

    Changes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,6 +4,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
+   config.secret_key = Rails.application.credentials.secret_key_base
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
